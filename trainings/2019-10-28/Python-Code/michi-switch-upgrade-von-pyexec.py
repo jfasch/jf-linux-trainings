f397393e8b57d4c66028e9b2a30f730792e43343
Joerg Faschingbauer <jf@faschingbauer.co.at>
2019-11-21 11:05:37 +0100

    2019-10-28: ...

--- trainings/2019-10-28/Python-Code/michi-switch-upgrade-von-pyexec.py
+++ trainings/2019-10-28/Python-Code/michi-switch-upgrade-von-pyexec.py
@@ -9,7 +9,6 @@ with open(switchimages) as f:
     fw_defs = {}
     exec(f.read(), fw_defs)
 
-
 print('sw_2960', fw_defs['sw_2960'])
 print('sw_2960s', fw_defs['sw_2960s'])
 print('sw_2960x', fw_defs['sw_2960x'])
